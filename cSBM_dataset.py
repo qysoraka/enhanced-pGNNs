ba322e36aaa8f68137a7247053348fc7c7cbffca
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-06-11 13:33:30 +0000

    Visual fixes

--- cSBM_dataset.py
+++ cSBM_dataset.py
@@ -18,3 +18,29 @@ contains functions:
 """
 import numpy as np
 import torch
+from torch_geometric.data import Data
+import pickle
+from datetime import datetime
+import os.path as osp
+import os
+import ipdb
+import argparse
+
+import torch
+from torch_geometric.data import InMemoryDataset
+
+
+
+def index_to_mask(index, size):
+    mask = torch.zeros(size, dtype=torch.bool, device=index.device)
+    mask[index] = 1
+    return mask
+
+
+def random_planetoid_splits(data, num_classes, percls_trn=20, val_lb=500, Flag=0):
+    # Set new random planetoid splits:
+    # * round(train_rate*len(data)/num_classes) * num_classes labels for training
+    # * val_rate*len(data) labels for validation
+    # * rest labels for testing
+
+    indices = []
