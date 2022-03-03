2894dc55dcc6ee8cc2d987ae5e887a84a7e38b28
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-03-03 22:05:54 +0000

    Added requirements.txt.

--- create_cSBM_dataset.sh
+++ create_cSBM_dataset.sh
@@ -82,4 +82,5 @@ python cSBM_dataset.py --phi 0.5 \
 python cSBM_dataset.py --phi 0.75 \
     --name csbm_phi_dense_0.75 \
     --root ./dataset \
-    --num_nodes 
+    --num_nodes 5000 \
+    --num_featu
