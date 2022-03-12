6fb3087ba42b1855f7321986a73da808a107686d
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-03-12 10:59:49 +0000

    tun: don't zeroize sock->file on detach

--- data_proc.py
+++ data_proc.py
@@ -6,3 +6,24 @@ import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid, WebKB, Actor, WikipediaNetwork, Amazon, Coauthor, CoraFull
 from torch_geometric.utils import num_nodes
 from cSBM_dataset import dataset_ContextualSBM
+
+def generate_split(data, num_classes, seed=2021, train_num_per_c=20, val_num_per_c=30):
+    np.random.seed(seed)
+    train_mask = torch.zeros(data.y.size(0), dtype=torch.bool)
+    val_mask = torch.zeros(data.y.size(0), dtype=torch.bool)
+    test_mask = torch.zeros(data.y.size(0), dtype=torch.bool)
+    for c in range(num_classes):
+        all_c_idx = (data.y == c).nonzero()
+        if all_c_idx.size(0) <= train_num_per_c + val_num_per_c:
+            test_mask[all_c_idx] = True
+            continue
+        perm = np.random.permutation(all_c_idx.size(0))
+        c_train_idx = all_c_idx[perm[:train_num_per_c]]
+        train_mask[c_train_idx] = True
+        test_mask[c_train_idx] = True
+        c_val_idx = all_c_idx[perm[train_num_per_c : train_num_per_c + val_num_per_c]]
+        val_mask[c_val_idx] = True
+        test_mask[c_val_idx] = True
+    test_mask = ~test_mask
+
+    return train_mask, val_mask, test_mask
