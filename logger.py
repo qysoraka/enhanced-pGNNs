b2a34bbc5673f14b2c0f1fd96ef84a50f5dd236d
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-06-15 07:07:55 +0000

    minor edits to readme

--- logger.py
+++ logger.py
@@ -14,4 +14,4 @@ class Logger(object):
     def print_statistics(self, run=None):
         if run is not None:
             result = 100 * torch.tensor(self.results[run])
-            argmax = result
+            argmax = result[:, 1].argmax().item
