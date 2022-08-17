be314ce99c67591301a6102f8fcee9d3e4d06cc4
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-08-17 21:47:23 +0000

    Improve install script

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ import torch_geometric.transforms as T
 
 def build_model(args, num_features, num_classes):
     if args.model == 'pgnn':
-        model = pGNNNet(in_cha
+        model = pGNNNet(in_channels=num_fea
