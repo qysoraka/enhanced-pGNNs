933814aa40dcc9a93ef15880ccbac51d85464605
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-08-18 11:47:26 +0000

    up readme

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ import torch_geometric.transforms as T
 def build_model(args, num_features, num_classes):
     if args.model == 'pgnn':
         model = pGNNNet(in_channels=num_features,
-                            out
+                            out_channels=num_c
