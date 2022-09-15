0fc032560c85a17295e2dbb6e134ba4b16246b71
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-09-15 20:54:48 +0000

    A bunch of changes

--- main.py
+++ main.py
@@ -24,4 +24,5 @@ def build_model(args, num_features, num_classes):
         model = GCNNet(in_channels=num_features,
                         out_channels=num_classes,
                         num_hid=args.num_hid,
-                        drop
+                        dropout=args.dropout)
+  
