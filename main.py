bac2f662911bec686984c9892d6f32cfec317aee
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-09-16 11:21:56 +0000

    added build status to readme

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def build_model(args, num_features, num_classes):
                         out_channels=num_classes,
                         num_hid=args.num_hid,
                         dropout=args.dropout)
-  
+    elif args.model ==
