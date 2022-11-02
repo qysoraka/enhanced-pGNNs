a997f90e5cc65e105fcbf0d3f8c795a3d4e73b6e
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-11-02 12:50:59 +0000

    Add .ruby-version file

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def build_model(args, num_features, num_classes):
     elif args.model == 'appnp':
         model = APPNPNet(in_channels=num_features,
                             out_channels=num_classes,
-                      
+                            num_hid=arg
