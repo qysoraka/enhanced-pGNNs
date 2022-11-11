9861be41240bdf3b7008f69a8a98e73c627b556f
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-11-11 14:45:31 +0000

    cosmetic fixes

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def build_model(args, num_features, num_classes):
                             num_hid=args.num_hid,
                             K=args.K,
                             alpha=args.alpha,
-         
+                            drop
