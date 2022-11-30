d717fcae8efe8392a0d1ed62738c158748e3abd6
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-11-30 15:29:47 +0000

    image fix

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ def build_model(args, num_features, num_classes):
         model = GPRGNNNet(in_channels=num_features,
                             out_channels=num_classes,
                             num_hid=args.num_hid,
-  
+                     
