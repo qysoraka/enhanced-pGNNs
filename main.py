f70698521aac50454716ec93f59924e1cd2d2b43
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-12-29 07:11:56 +0000

    Fix comment

--- main.py
+++ main.py
@@ -59,4 +59,5 @@ def build_model(args, num_features, num_classes):
                             Init=args.Init,
                             Gamma=args.Gamma,
                             dprate=args.dprate,
-                            dropout=args
+                            dropout=args.dropout)
+ 
