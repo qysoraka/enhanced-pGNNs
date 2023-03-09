6d82141a9c8d2eb75d054482d9a944b2f6ef366f
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-03-09 17:08:06 +0000

    checking how to get results

--- main.py
+++ main.py
@@ -82,4 +82,4 @@ def test(model, data):
 def main(args):
     print(args)
     data, num_features, num_classes = load_data(args, rand_seed=2021)
-    device = torch.device('c
+    device = torch.device('cuda' if torch.cuda.is
