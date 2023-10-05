3697aad162647b665717fcd459f6fa17f4b5b0d8
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-10-05 14:52:22 +0000

    Updating Readme.

--- main.py
+++ main.py
@@ -114,3 +114,4 @@ def get_args():
                         default='cora',                    
                         help='Input graph.')
     parser.add_argument('--train_rate', 
+               
