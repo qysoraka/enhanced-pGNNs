edc3e7ea56ed7771518c29e42417af5ebf5dceea
qysoraka <156439778+qysoraka@users.noreply.github.com>
2023-11-28 08:06:51 +0000

    Fixed issue #13

--- main.py
+++ main.py
@@ -148,4 +148,4 @@ def get_args():
                         help='Number of hidden units.')
     parser.add_argument('--dropout', 
                         type=float, 
-               
+                        default=0.
