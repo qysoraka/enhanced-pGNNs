61c76d7051e311e5e0be2df8a655756e89d115db
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-02-25 09:53:03 +0000

    String fix

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,5 @@ function run_exp(){
     python main.py --input $1 \
                         --train_rate $2 \
                         --val_rate $3 \
-                        --m
+                        --model $4 \
+          
