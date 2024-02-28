8401484270e5a9f63b16d9ef3c6b67224b6e5cd5
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-02-28 18:21:05 +0000

    bo

--- run_test.sh
+++ run_test.sh
@@ -9,4 +9,5 @@ function run_exp(){
                         --lr $6 \
                         --epochs $7 \
                         --weight_decay $8 \
-                     
+                        --dropout $9 \
+           
