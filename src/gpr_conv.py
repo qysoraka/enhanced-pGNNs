6019dd19d208746d8b28e9b26cb88245237a663c
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-03-18 05:55:34 +0000

    #52

---
+++ src/gpr_conv.py
@@ -0,0 +1,13 @@
+
+from typing import Optional, Tuple
+import numpy as np
+from torch._C import BenchmarkExecutionStats
+from torch_geometric.typing import Adj, OptTensor, PairTensor
+
+import torch
+from torch import Tensor
+from torch.nn import Parameter
+from torch_scatter import scatter_add
+from torch_sparse import SparseTensor, matmul, fill_diag, sum, mul
+
+from torch_geometric.nn import MessagePassing
