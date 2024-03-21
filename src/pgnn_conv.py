219e00ea2dd78531d332ef568f8ed7cbddbafdcd
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-03-21 22:27:07 +0000

    test correction

--- src/pgnn_conv.py
+++ src/pgnn_conv.py
@@ -3,3 +3,15 @@ from typing import Optional, Tuple
 
 from torch._C import BenchmarkExecutionStats
 from torch_geometric.typing import Adj, OptTensor, PairTensor
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import Parameter
+from torch_scatter import scatter_add
+from torch_sparse import SparseTensor, matmul, fill_diag, sum, mul
+
+from torch_geometric.utils import num_nodes
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_remaining_self_loops
+from torch_geometric.utils.num_nodes import maybe_num_nodes
