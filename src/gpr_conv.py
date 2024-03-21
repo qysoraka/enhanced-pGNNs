1f85646da7d21e9e9d18b7cfbce4547df701b489
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-03-21 13:17:10 +0000

    remove temporary files

--- src/gpr_conv.py
+++ src/gpr_conv.py
@@ -51,3 +51,24 @@ class GPR_prop(MessagePassing):
 
     def reset_parameters(self):
         torch.nn.init.zeros_(self.temp)
+        for k in range(self.K+1):
+            self.temp.data[k] = self.alpha*(1-self.alpha)**k
+        self.temp.data[-1] = (1-self.alpha)**self.K
+
+    def forward(self, x, edge_index, edge_weight=None):
+        edge_index, norm = gcn_norm(
+            edge_index, edge_weight, num_nodes=x.size(0), dtype=x.dtype)
+
+        hidden = x*(self.temp[0])
+        for k in range(self.K):
+            x = self.propagate(edge_index, x=x, norm=norm)
+            gamma = self.temp[k+1]
+            hidden = hidden + gamma*x
+        return hidden
+
+    def message(self, x_j, norm):
+        return norm.view(-1, 1) * x_j
+
+    def __repr__(self):
+        return '{}(K={}, temp={})'.format(self.__class__.__name__, self.K,
+                                          self.temp)
---
+++ src/pgnn_conv.py
@@ -0,0 +1,5 @@
+
+from typing import Optional, Tuple
+
+from torch._C import BenchmarkExecutionStats
+from torch_geometric.typing import Adj, OptTensor, PairTensor
