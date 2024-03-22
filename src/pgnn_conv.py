018706e7012e8242466f77f148fddf67f2a8979b
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-03-22 07:36:43 +0000

    Fix last change.

--- src/pgnn_conv.py
+++ src/pgnn_conv.py
@@ -15,3 +15,18 @@ from torch_geometric.utils import num_nodes
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_remaining_self_loops
 from torch_geometric.utils.num_nodes import maybe_num_nodes
+
+from torch_geometric.nn.inits import glorot, zeros
+
+
+@torch.jit._overload
+def pgnn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+             add_self_loops=True, dtype=None):
+    # type: (Tensor, OptTensor, Optional[int], bool, bool, Optional[int]) -> PairTensor  # noqa
+    pass
+
+
+@torch.jit._overload
+def pgnn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+             add_self_loops=True, dtype=None):
+    # type: (SparseTensor, OptTensor, Optional[int], bool, bool, Optional[int]) -> SparseTensor  # noqa
