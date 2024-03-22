4043410eeb9a3ed5f7bf189db77dd095ab1d199e
qysoraka <156439778+qysoraka@users.noreply.github.com>
2024-03-22 07:47:52 +0000

    Bump to next dev version

--- src/pgnn_conv.py
+++ src/pgnn_conv.py
@@ -30,3 +30,21 @@ def pgnn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
 def pgnn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
              add_self_loops=True, dtype=None):
     # type: (SparseTensor, OptTensor, Optional[int], bool, bool, Optional[int]) -> SparseTensor  # noqa
+    pass
+
+
+def pgnn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+             add_self_loops=False, dtype=None):
+
+    fill_value = 2. if improved else 1.
+
+    if isinstance(edge_index, SparseTensor):
+        adj_t = edge_index
+        if not adj_t.has_value():
+            adj_t = adj_t.fill_value(1., dtype=dtype)
+        if add_self_loops:
+            adj_t = fill_diag(adj_t, fill_value)
+        deg = sum(adj_t, dim=1)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
+        adj_t = mul(adj_t, deg_inv_sqrt.view(-1, 1))
